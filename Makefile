8ec54c5beed57e4885140473db25cb89c0cfbe43
rounakdatta <rounakdatta12@gmail.com>
2018-04-10 10:02:40 +0530

    dp part 1

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-OBJECTS = array array backtracking binary binary-search-tree binary-tree divide-conquer dynamic-programming greedy heaps matrix puzzles queue sorting stack string trie
+OBJECTS = array backtracking binary binary-search-tree binary-tree divide-conquer dynamic-programming greedy heaps matrix puzzles queue sorting stack string trie
 
 $(OBJECTS):
 	cd $@; g++ $@.cpp -o $@; \
---
+++ dynamic-programming/one.h
@@ -0,0 +1,32 @@
+// find the longest common subsequence using dynamic programming
+
+#include <iostream>
+#include <string.h>
+#define MAX(a, b) ((a > b)? a : b)
+
+using namespace std;
+
+int lcs(string a, string b, int m, int n)
+{
+	if(m == 0 || n == 0)
+	{
+		return 0;
+	}
+	if(a[m - 1] == b[n - 1])
+	{
+		return 1 + lcs(a, b, m - 1, n - 1);
+	}
+	else
+	{
+		return (MAX(lcs(a, b, m - 1, n), lcs(a, b, m, n - 1)));
+	}
+}
+
+int one()
+{
+	string word1, word2;
+	cin >> word1 >> word2;
+	cout << "LCS : " << lcs(word1, word2, word1.length(), word2.length());
+
+	return 0;
+}
